313f7c638b1ce8837bb45e7b5a89aaffb4be3273
kwj <kwj@vlax.net>
2024-12-07 03:32:38 -0800

    Rename a variable

--- day_07/d07.jl
+++ day_07/d07.jl
@@ -13,33 +13,31 @@ function parse_file(fname::String)
 end
 
 function d07_p1(fname::String = "input")
-    function eval_p1(eq::Equation)
-        @assert length(eq.numbers) > 1 "invalid data"
+    function eval_p1(eqn::Equation)
+        @assert length(eqn.numbers) > 1 "invalid data"
 
-        reduce(eq.numbers[2:end]; init = [eq.numbers[1]]) do xs, n
+        reduce(eqn.numbers[2:end]; init = [eqn.numbers[1]]) do xs, n
             Iterators.flatmap(x -> [x * n, x + n], xs) |>
                 collect |>
-                filter(x -> x <= eq.value)
+                filter(x -> x <= eqn.value)
         end
     end
 
-    data = parse_file(fname)
-    map(x -> x.value, filter(eq -> eq.value ∈ eval_p1(eq), data)) |> sum
+    map(x -> x.value, filter(eq -> eq.value ∈ eval_p1(eq), parse_file(fname))) |> sum
 end
 
 function d07_p2(fname::String = "input")
-    function eval_p2(eq::Equation)
-        @assert length(eq.numbers) > 1 "invalid data"
+    function eval_p2(eqn::Equation)
+        @assert length(eqn.numbers) > 1 "invalid data"
 
-        reduce(eq.numbers[2:end]; init = [eq.numbers[1]]) do xs, n
+        reduce(eqn.numbers[2:end]; init = [eqn.numbers[1]]) do xs, n
             Iterators.flatmap(x -> [x * n, x + n, parse(Int, string(x) * string(n))], xs) |>
                 collect |>
-                filter(x -> x <= eq.value)
+                filter(x -> x <= eqn.value)
         end
     end
 
-    data = parse_file(fname)
-    map(x -> x.value, filter(eq -> eq.value ∈ eval_p2(eq), data)) |> sum
+    map(x -> x.value, filter(eq -> eq.value ∈ eval_p2(eq), parse_file(fname))) |> sum
 end
 
 end #module
