e1539edd7c6e00f72859d9e095fe2f4ce434d2f8
kwj <kwj@vlax.net>
2024-12-10 10:17:46 -0800

    Add a completion message at initialization

--- solve.sh
+++ solve.sh
@@ -3,7 +3,7 @@
 base_dir=$(cd $(dirname $0); pwd)
 
 if [ $# -eq 1 -a "$1" = "init" ]; then
-    julia --project=${base_dir} --eval 'using Pkg; Pkg.instantiate()'
+    julia --project=${base_dir} --eval 'using Pkg; Pkg.instantiate()' && echo 'Done'
 else
     julia --project=${base_dir} --interactive --eval 'using AoC2024'
 fi
