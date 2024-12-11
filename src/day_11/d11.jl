130c0620d24e98683e59a2529c5ccbb4e58593d2
kwj <kwj@vlax.net>
2024-12-11 08:30:34 -0800

    Use divrem()

--- src/day_11/d11.jl
+++ src/day_11/d11.jl
@@ -21,9 +21,7 @@ function blink(cm::Dict{Int64, Int64}, cnt::Int)
             if iszero(k)
                 new_cm[1] = get(new_cm, 1, 0) + v
             elseif iseven(ndigits(k))
-                len = div(ndigits(k), 2)
-                left = parse(Int64, string(k)[1:len])
-                right = parse(Int64, string(k)[len + 1:end])
+                left, right = divrem(k, 10 ^ div(ndigits(k), 2))
                 new_cm[left] = get(new_cm, left, 0) + v
                 new_cm[right] = get(new_cm, right, 0) + v
             else
