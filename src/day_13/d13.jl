1b0ad5cffc46a3abaedb04e3a2ed8f7c028d57f6
kwj <kwj@vlax.net>
2024-12-13 11:45:50 -0800

    Add comments

--- src/day_13/d13.jl
+++ src/day_13/d13.jl
@@ -12,22 +12,27 @@ function parse_file(fname::String)
 end
 
 function play(claw1::Tuple{Int64, Int64, Int64}, claw2::Tuple{Int64, Int64, Int64})
-    a1, b1, c1 = claw1
-    a2, b2, c2 = claw2
-
     # two linear Diophantine equations
     #   a1 * A + b1 * B = c1
     #   a2 * A + b2 * B = c2
+    a1, b1, c1 = claw1
+    a2, b2, c2 = claw2
+
+    # Each equation must have integer solutions
     if !iszero(rem(c1, gcd(a1, b1))) || !iszero(rem(c2, gcd(a2, b2)))
-        nothing
+        return nothing
+    end
+
+    # If `b1 * a2 - b2 * a1` is equal to 0, another processes is required.
+    # Fortunately, there were no such cases in the given data, so it cut corners.
+    B = (c1 * a2 - c2 * a1) / (b1 * a2 - b2 * a1)
+    A = (c1 - b1 * B) / a1
+
+    # The coordinates of the intersection of two lines must be integers
+    if isinteger(A) && isinteger(B)
+        Int64(A), Int64(B)
     else
-        B = (c1 * a2 - c2 * a1) / (b1 * a2 - b2 * a1)
-        A = (c1 - b1 * B) / a1
-        if isinteger(A) && isinteger(B)
-            Int64(A), Int64(B)
-        else
-            nothing
-        end
+        nothing
     end
 end
 
