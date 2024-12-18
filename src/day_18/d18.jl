e7f775f54fd3dc7b65a6aab920508338d4030360
kwj <kwj@vlax.net>
2024-12-18 00:36:30 -0800

    Use binary search to improve performance

--- src/day_18/d18.jl
+++ src/day_18/d18.jl
@@ -1,10 +1,6 @@
 
 module Day18
 
-#
-# TODO: To improve performance, try the binary search in part two.
-#
-
 import DataStructures: PriorityQueue, dequeue!
 
 const CIdx = CartesianIndex
@@ -15,7 +11,7 @@ const Y_SIZE = 71
 
 function parse_file(fname::String)
     data = parse.(Int, stack(split.(readlines(joinpath(@__DIR__, fname)), ","), dims = 1)) .+ 1
-    zip(data[:, 1], data[:, 2])
+    zip(data[:, 1], data[:, 2]) |> collect
 end
 
 function dijkstra(grid::Array{Int, 2}, start::CIdx{2}, goal::CIdx{2})
@@ -26,12 +22,11 @@ function dijkstra(grid::Array{Int, 2}, start::CIdx{2}, goal::CIdx{2})
     pq = PriorityQueue{CIdx{2}, Int}()
     pq[start] = 0
 
-    found = false
     while !isempty(pq)
         ci = dequeue!(pq)
         if ci == goal
-            found = true
-            break
+            # Return the number of steps to the goal
+            return dest_tbl[goal]
         end
 
         for adj in filter(x -> checkbounds(Bool, grid, x) && iszero(grid[x]), ci .+ DIRS)
@@ -44,68 +39,48 @@ function dijkstra(grid::Array{Int, 2}, start::CIdx{2}, goal::CIdx{2})
         end
     end
 
-    if found
-        dest_tbl[goal], get_route(prev_tbl, goal)
-    else
-        0, nothing
-    end
-end
-
-function get_route(prev_tbl::Array{Union{Nothing, CIdx{2}}, 2}, goal::CIdx{2})
-    route = Vector{NTuple{2, Int}}()
-    push!(route, Tuple(goal))
-
-    next_ci = prev_tbl[goal]
-    while !isnothing(next_ci)
-        push!(route, Tuple(next_ci))
-        next_ci = prev_tbl[next_ci]
-    end
-
-    route
+    # There is no path to the goal.
+    0
 end
 
 function d18_p1(fname::String = "input")
+    start = CIdx(1, 1)
+    goal = CIdx(X_SIZE, Y_SIZE)
     grid = zeros(Int, X_SIZE, Y_SIZE)
 
-    for pos in Iterators.take(parse_file(fname), 1024)
+    for pos in parse_file(fname)[1:1024]
         grid[pos...] = 1
     end
 
-    start = CIdx(1, 1)
-    goal = CIdx(X_SIZE, Y_SIZE)
-    steps, _ = dijkstra(grid, start, goal)
-
-    steps
+    dijkstra(grid, start, goal)
 end
 
 function d18_p2(fname::String = "input")
-    grid = zeros(Int, X_SIZE, Y_SIZE)
-    byte_itr = parse_file(fname)
-
-    for pos in Iterators.take(byte_itr, 1024)
-        grid[pos...] = 1
-    end
-
     start = CIdx(1, 1)
     goal = CIdx(X_SIZE, Y_SIZE)
-    _, route = dijkstra(grid, start, goal)
+    grid = zeros(Int, X_SIZE, Y_SIZE)
 
-    route_set = Set(route)
-    for pos in Iterators.drop(byte_itr, 1024)
-        grid[pos...] = 1
-        if pos ∉ route_set
-            continue
+    falling_bytes = parse_file(fname)
+
+    # binary search
+    L = 1024
+    R = length(falling_bytes)
+    while L + 1 < R
+        fill!(grid, 0)
+
+        m = div(L + R, 2)
+        for pos in falling_bytes[1:m]
+            grid[pos...] = 1
         end
-
-        steps, route = dijkstra(grid, start, goal)
-        if iszero(steps)
-            x, y = pos .- 1
-            println(x, ",", y)
-            break
+        if iszero(dijkstra(grid, start, goal))
+            R = m
         else
-            route_set = Set(route)
+            L = m
         end
     end
+
+    x, y = falling_bytes[R] .- 1
+    println(x, ",", y)
 end
 
 end #module
