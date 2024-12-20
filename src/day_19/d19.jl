4341fbc06f2b116a9482bbf40e79f1fe20d32f95
kwj <kwj@vlax.net>
2024-12-19 17:43:55 -0800

    Use composite types for clarity

--- src/day_19/d19.jl
+++ src/day_19/d19.jl
@@ -1,31 +1,36 @@
 
 module Day19
 
+struct Towel
+    pattern::String
+    len::Int64
+end
+
 function parse_file(fname::String)
     data = readlines(joinpath(@__DIR__, fname))
 
-    towels = split(data[1], ", ")
+    towels = map(t -> Towel(t, length(t)), split(data[1], ", "))
     designs = data[3:end]
 
     towels, designs
 end
 
-function is_possible(d::AbstractString, towels::Vector{SubString{String}})
+function is_possible(d::AbstractString, towels::Vector{Towel})
     iszero(length(d)) || any(towels) do x
-        if startswith(d, x)
-            is_possible((@view d[length(x) + 1:end]), towels)
+        if startswith(d, x.pattern)
+            is_possible((@view d[x.len + 1:end]), towels)
         else
             false
         end
     end
 end
 
-function count_combinations(d::AbstractString, towels::Vector{SubString{String}}, memo::Dict{String, Int64})
+function count_combinations(d::AbstractString, towels::Vector{Towel}, memo::Dict{String, Int64})
     # I don't know the reason why using the `get!()` function increases number of memory allocations.
     if !haskey(memo, d)
         memo[d] = sum(towels) do x
-            if startswith(d, x)
-                count_combinations((@view d[length(x) + 1:end]), towels, memo)
+            if startswith(d, x.pattern)
+                count_combinations((@view d[x.len + 1:end]), towels, memo)
             else
                 0
             end
