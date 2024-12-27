3053102ee814f7f6fc2083e98e3385353ae83b0f
kwj <kwj@vlax.net>
2024-12-27 10:54:40 -0800

    Use a naive algorithm

--- src/day_09/d09.jl
+++ src/day_09/d09.jl
@@ -1,108 +1,99 @@
 
 module Day09
 
-mutable struct Block
-    id::Int64  # 0: free space, positive numbers (> 0): file
-    len::Int64
-end
-
-Base.copy(s::Block) = Block(s.id, s.len)
-
 function parse_file(fname::String)
-    parse.(Int64, split(readlines(joinpath(@__DIR__, fname)) |> first, ""))
+    data = parse.(Int64, split(chomp(read(joinpath(@__DIR__, fname), String)), ""))
+    file_capacities = data[1:2:end]
+    file_IDs = collect(0:(length(file_capacities) - 1))
+    gap_capacities = data[2:2:end]
+
+    file_capacities, file_IDs, gap_capacities
 end
 
-# Note: the `blocks` parameter of the caller will be modified
-function trim_blocks!(blocks::Vector{Block})
-    while iszero(blocks[end].id) || iszero(blocks[end].len)
-        pop!(blocks)
-    end
-
-    blocks
-end
-
-function make_block_lst(lst::Vector{Int64})
-    map(collect(enumerate(lst))) do (idx, len)
-        if isodd(idx)
-            Block((idx + 1) ÷ 2, len)
-        else
-            Block(0, len)
-        end
-    end |> filter(blk -> blk.len != 0) |> trim_blocks!
-end
-
-function checksum(blocks::Vector{Block})
-    acc = 0
-    idx = 0
-    for blk in blocks
-        if !iszero(blk.id)
-            # Actual ID number is one less than `Block.id`
-            acc += sum((blk.id - 1) .* collect(idx:(idx + blk.len - 1)))
-        end
-        idx += blk.len
-    end
-
-    acc
+function blk_chksum(pos::Int64, id::Int64, len::Int64)
+    sum(p -> p * id, pos:(pos + len - 1))
 end
 
 function d09_p1(fname::String = "input")
-    blocks = make_block_lst(parse_file(fname))
+    file_capacities, file_IDs, gap_capacities = parse_file(fname)
 
-    i = 1
-    while i < length(blocks)
-        if iszero(blocks[i].id)
-            if blocks[i].len <= blocks[end].len
-                blocks[i].id = blocks[end].id
-                blocks[end].len -= blocks[i].len
-            else
-                blocks[i].len -= blocks[end].len
-                insert!(blocks, i, pop!(blocks))
+    chksum = 0
+    pos = 0
+    idx = 1  # Use for both file_capacities and gap_capacities
+    last_idx = lastindex(file_capacities)
+
+    while idx <= last_idx
+        # file block
+        if file_capacities[idx] > 0
+            chksum += blk_chksum(pos, file_IDs[idx], file_capacities[idx])
+            pos += file_capacities[idx]
+        end
+
+        # free space block (gap)
+        if checkbounds(Bool, gap_capacities, idx)
+            while gap_capacities[idx] > 0
+                last_idx = findlast(>(0), file_capacities)
+                if last_idx <= idx
+                    break
+                end
+
+                len = min(gap_capacities[idx], file_capacities[last_idx])
+                chksum += blk_chksum(pos, file_IDs[last_idx], len)
+                pos += len
+                gap_capacities[idx] -= len
+                file_capacities[last_idx] -= len
             end
         end
 
-        # Truncate space blocks and empty blocks at the end of block list
-        # to ensure the last block, `blocks[end]`, is a file block
-        trim_blocks!(blocks)
-
-        i += 1
+        idx += 1
     end
 
-    checksum(blocks)
+    chksum
 end
 
 function d09_p2(fname::String = "input")
-    function find_space_block(lst::Vector{Block}, src_blk_idx::Int64)
-        space_blk_idx = findfirst(blk -> iszero(blk.id) && lst[src_blk_idx].len <= blk.len, lst)
-        if !isnothing(space_blk_idx) && space_blk_idx < src_blk_idx
-            space_blk_idx
-        else
-            nothing
-        end
-    end
+    file_capacities, file_IDs, gap_capacities = parse_file(fname)
+    exist_file = trues(length(file_IDs))
 
-    blocks = make_block_lst(parse_file(fname))
+    chksum = 0
+    pos = 0
+    idx = 1  # Use for both file_capacities and gap_capacities
 
-    src_idx = lastindex(blocks)
-    while src_idx > 1
-        if !iszero(blocks[src_idx].id)
-            dest_idx = find_space_block(blocks, src_idx)
-            if !isnothing(dest_idx)
-                if blocks[src_idx].len == blocks[dest_idx].len
-                    blocks[dest_idx].id = blocks[src_idx].id
-                    blocks[src_idx].id = 0
-                else
-                    blocks[dest_idx].len -= blocks[src_idx].len
-                    insert!(blocks, dest_idx, copy(blocks[src_idx]))
-                    src_idx += 1
-                    blocks[src_idx].id = 0
-                end
+    while idx <= lastindex(file_capacities)
+        # file block
+        if file_capacities[idx] > 0
+            # if the block is not exist, skip to the next block
+            if exist_file[idx]
+                chksum += blk_chksum(pos, file_IDs[idx], file_capacities[idx])
             end
+            pos += file_capacities[idx]
         end
 
-        src_idx -= 1
+        # free space block (gap)
+        if checkbounds(Bool, gap_capacities, idx)
+            while gap_capacities[idx] > 0
+                move_idx = lastindex(file_capacities)
+                while idx < move_idx
+                    if 0 < file_capacities[move_idx] <= gap_capacities[idx] && exist_file[move_idx]
+                        break
+                    end
+                    move_idx -= 1
+                end
+                move_idx <= idx && break
+
+                len = file_capacities[move_idx]
+                chksum += blk_chksum(pos, file_IDs[move_idx], len)
+                pos += len
+                gap_capacities[idx] -= len
+                exist_file[move_idx] = false
+            end
+            pos += gap_capacities[idx]
+        end
+
+        idx += 1
     end
 
-    checksum(blocks)
+    chksum
 end
 
 end #module
