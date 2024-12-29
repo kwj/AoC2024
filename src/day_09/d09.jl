799b1f02eded0a5d5b8bb3789bdfe7f02fccf46c
kwj <kwj@vlax.net>
2024-12-28 16:33:15 -0800

    Rename variables

--- src/day_09/d09.jl
+++ src/day_09/d09.jl
@@ -3,44 +3,47 @@ module Day09
 
 function parse_file(fname::String)
     data = parse.(Int64, split(chomp(read(joinpath(@__DIR__, fname), String)), ""))
-    file_capacities = data[1:2:end]
-    gap_capacities = data[2:2:end]
+    file_blocks = data[1:2:end]
+    gap_blocks = data[2:2:end]
 
-    file_capacities, gap_capacities
+    file_blocks, gap_blocks
 end
+
+file_ID(idx::Int64) = idx - 1
 
 function blk_chksum(pos::Int64, id::Int64, len::Int64)
     sum(p -> p * id, pos:(pos + len - 1))
 end
 
 function d09_p1(fname::String = "input")
-    file_capacities, gap_capacities = parse_file(fname)
+    file_blocks, gap_blocks = parse_file(fname)
 
     chksum = 0
     pos = 0
-    idx = 1  # Use for both file_capacities and gap_capacities
-    last_idx = lastindex(file_capacities)
+    idx = 1  # Use for both file_blocks and gap_blocks
+    last_file_idx = lastindex(file_blocks)
 
-    while idx <= last_idx
+    while idx <= last_file_idx
         # file block
-        if file_capacities[idx] > 0
-            chksum += blk_chksum(pos, (idx - 1), file_capacities[idx])
-            pos += file_capacities[idx]
+        if file_blocks[idx] > 0
+            chksum += blk_chksum(pos, file_ID(idx), file_blocks[idx])
+            pos += file_blocks[idx]
         end
 
         # free space block (gap)
-        if checkbounds(Bool, gap_capacities, idx)
-            while gap_capacities[idx] > 0
-                last_idx = findlast(>(0), file_capacities)
-                if last_idx <= idx
-                    break
+        if checkbounds(Bool, gap_blocks, idx)
+            while gap_blocks[idx] > 0
+                # skip empty file blocks
+                while file_blocks[last_file_idx] == 0
+                    last_file_idx -= 1
                 end
+                last_file_idx <= idx && break
 
-                len = min(gap_capacities[idx], file_capacities[last_idx])
-                chksum += blk_chksum(pos, (last_idx - 1), len)
+                len = min(gap_blocks[idx], file_blocks[last_file_idx])
+                chksum += blk_chksum(pos, file_ID(last_file_idx), len)
                 pos += len
-                gap_capacities[idx] -= len
-                file_capacities[last_idx] -= len
+                gap_blocks[idx] -= len
+                file_blocks[last_file_idx] -= len
             end
         end
 
@@ -51,42 +54,42 @@ function d09_p1(fname::String = "input")
 end
 
 function d09_p2(fname::String = "input")
-    file_capacities, gap_capacities = parse_file(fname)
-    exist_file = trues(length(file_capacities))
+    file_blocks, gap_blocks = parse_file(fname)
+    exist_file = trues(length(file_blocks))
 
     chksum = 0
     pos = 0
-    idx = 1  # Use for both file_capacities and gap_capacities
+    idx = 1  # Use for both file_blocks and gap_blocks
 
-    while idx <= lastindex(file_capacities)
+    while idx <= lastindex(file_blocks)
         # file block
-        if file_capacities[idx] > 0
+        if file_blocks[idx] > 0
             # if the block is not exist, skip to the next block
             if exist_file[idx]
-                chksum += blk_chksum(pos, (idx - 1), file_capacities[idx])
+                chksum += blk_chksum(pos, file_ID(idx), file_blocks[idx])
             end
-            pos += file_capacities[idx]
+            pos += file_blocks[idx]
         end
 
         # free space block (gap)
-        if checkbounds(Bool, gap_capacities, idx)
-            while gap_capacities[idx] > 0
-                move_idx = lastindex(file_capacities)
-                while idx < move_idx
-                    if 0 < file_capacities[move_idx] <= gap_capacities[idx] && exist_file[move_idx]
+        if checkbounds(Bool, gap_blocks, idx)
+            while gap_blocks[idx] > 0
+                move_src_idx = lastindex(file_blocks)
+                while idx < move_src_idx
+                    if 0 < file_blocks[move_src_idx] <= gap_blocks[idx] && exist_file[move_src_idx]
                         break
                     end
-                    move_idx -= 1
+                    move_src_idx -= 1
                 end
-                move_idx <= idx && break
+                move_src_idx <= idx && break
 
-                len = file_capacities[move_idx]
-                chksum += blk_chksum(pos, (move_idx - 1), len)
+                len = file_blocks[move_src_idx]
+                chksum += blk_chksum(pos, file_ID(move_src_idx), len)
                 pos += len
-                gap_capacities[idx] -= len
-                exist_file[move_idx] = false
+                gap_blocks[idx] -= len
+                exist_file[move_src_idx] = false
             end
-            pos += gap_capacities[idx]
+            pos += gap_blocks[idx]
         end
 
         idx += 1
